a27f37d3428e8e8a82f62cd023bd135fd5957c16
Riya Kapoor <riya_tt@outlook.com>
2023-09-01 12:08:00 +0100

    exercise 2 completed day 4

---
+++ javascript/conditionals.js
@@ -0,0 +1,18 @@
+let age = 10;
+
+if (age <= 17) {
+console.log("Underage")
+
+}
+
+else { console.log("18 or over")
+
+}
+
+if (age <= 17) {
+    console.log("Underage");
+} else if ( age <= 65 ) {
+    console.log("Insurable");
+} else {
+    console.log("out of range");
+}
